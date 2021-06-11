a226e563882a1124a01991c1a8dbc7f15b993d5c
Digvijay <digvijaysinghonly1@gmail.com>
2021-06-11 15:05:14 +0530

    searchBox outline removed

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -8,7 +8,7 @@ const name=require('@rstacruz/startup-name-generator');
 
 class App extends React.Component{
     state={
-        headerText:'Name It!',
+        headerText:'Name Generator',
         headerExpanded: true,
         suggestedNames:[]
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,8 @@ const Header=({headTitle,headerExpanded})=>{
     return (
         <div className="head-container">
         <img src="https://user-images.githubusercontent.com/26179770/106359099-693e3380-6336-11eb-8069-9e36f25de5ca.png"
-        className={`head-image
+        
+        target="_blank" className={`head-image
         ${headerExpanded ?'head-image-expanded':'head-image-contracted'}` }
         alt="headerImage">
 
